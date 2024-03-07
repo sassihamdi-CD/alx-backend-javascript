1a003fb95ac7e4632a76b61e98855f34251f8dcf
sassihamdi-CD <hamdysassy7@gmail.com>
2024-03-07 23:28:39 +0100

    bug fix

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,9 @@
 import ClassRoom from './0-classroom';
 
-export default function(){
-	return [
-		new ClassRoom(10),
-		new ClassRoom(20),
-		new ClassRoom(34),
-	
-	];
+export default function initializeRooms() {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
